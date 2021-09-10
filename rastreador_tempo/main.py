648105044e059bf0eff9bd40d137b21bd0875aa1
rafaelmatsumoto <rafaelmatsumoto43@gmail.com>
2021-09-09 22:28:04 -0300

    feat: add name of activity and hourly rate to the command line

--- rastreador_tempo/main.py
+++ rastreador_tempo/main.py
@@ -6,12 +6,14 @@ app = typer.Typer()
 
 
 @app.command()
-def main():
+def main(hourly_rate: float, activity: str):
     start_time = datetime.now()
     while True:
         sleep(1)
         current_time = datetime.now() - start_time
-        print(current_time, end="", flush=True)
+        print(f"Activity: {activity} "
+              f"- Current time tracked: {current_time} "
+              f"- Billed time: ${round(current_time.total_seconds() * (hourly_rate/3600), 2)}", end="", flush=True)
         print("\r", end="", flush=True)
 
 
