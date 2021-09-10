36aa94f8c38d7ac4f687b25a971a694b236b00f2
rafaelmatsumoto <rafaelmatsumoto43@gmail.com>
2021-09-09 23:05:42 -0300

    feat: add report feature to the application

--- rastreador_tempo/main.py
+++ rastreador_tempo/main.py
@@ -1,4 +1,6 @@
 import typer
+import sys
+import csv
 from datetime import datetime
 from time import sleep
 
@@ -9,12 +11,24 @@ app = typer.Typer()
 def main(hourly_rate: float, activity: str):
     start_time = datetime.now()
     while True:
-        sleep(1)
-        current_time = datetime.now() - start_time
-        print(f"Activity: {activity} "
-              f"- Current time tracked: {current_time} "
-              f"- Billed time: ${round(current_time.total_seconds() * (hourly_rate/3600), 2)}", end="", flush=True)
-        print("\r", end="", flush=True)
+        try:
+            sleep(1)
+            current_time = datetime.now() - start_time
+            billed_time = current_time.total_seconds() * (hourly_rate/3600)
+            print(f"Activity: {activity} "
+                  f"- Current time tracked: {current_time} "
+                  f"- Billed time: ${round(billed_time, 2)}", end="", flush=True)
+            print("\r", end="", flush=True)
+        except KeyboardInterrupt:
+            with open('report.csv', 'w', newline='') as file:
+                fieldnames = ['activity', 'current_time', 'billed_time']
+                writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+                writer.writeheader()
+                writer.writerow({'activity': activity, 'current_time': current_time, 'billed_time': billed_time})
+
+            print("\nBye")
+            sys.exit()
 
 
 if __name__ == "__main__":
