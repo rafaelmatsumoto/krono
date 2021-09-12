ff6c4a6ca9ea65e5cc848cd163a4d89287a6f3e7
rafaelmatsumoto <rafaelmatsumoto43@gmail.com>
2021-09-12 15:00:37 -0300

    feat: add config file to time tracker

--- rastreador_tempo/main.py
+++ rastreador_tempo/main.py
@@ -4,24 +4,35 @@ import sys
 import csv
 import pathlib
 from typing import Optional
-from datetime import datetime
+from datetime import datetime, date
 from time import sleep
 
 app = typer.Typer()
 
 
 @app.command()
-def main(hourly_rate: float, activity: str,
+def main(activity: str,
+         hourly_rate: Optional[float] = typer.Option(None, "--hourly_rate", "-h", help="Hourly rate"),
          currency: Optional[str] = typer.Option("USD", "--currency", "-c", help="Currency used"),
          requested_from: Optional[str] = typer.Option(None, "--requested_from", "-r",
                                                       help="Developer/Company requesting payment"),
          bill_to: Optional[str] = typer.Option(None, "--bill_to", "-b", help="Company to bill to"),
          finish_report: Optional[bool] = typer.Option(False, "--finish_report", "-f",
-                                                      help="Add business metadata to the final version of the report")):
+                                                      help="Add business metadata to the final version of the report"),
+         due_date: Optional[str] = typer.Option(None, "--due_date", "-d", help="Due date for payment")):
     file_path = pathlib.Path.cwd() / "rastreador.json"
     if file_path.is_file():
         with open(file_path, mode='r+') as fid:
             data = json.load(fid)
+            if hourly_rate is None:
+                hourly_rate = data["hourly_rate"]
+
+            if finish_report:
+                if requested_from is None:
+                    requested_from = data["requested_from"]
+
+                if bill_to is None:
+                    bill_to = data["bill_to"]
 
     start_time = datetime.now()
     print("Press CTRL+C to stop timer")
@@ -29,10 +40,10 @@ def main(hourly_rate: float, activity: str,
         try:
             sleep(1)
             current_time = datetime.now() - start_time
-            billed_time = current_time.total_seconds() * (hourly_rate/3600)
+            billed_time = round(current_time.total_seconds() * (hourly_rate/3600), 2)
             print(f"Activity: {activity} "
                   f"- Current time tracked: {current_time} "
-                  f"- Billed time: ${round(billed_time, 2)}", end="", flush=True)
+                  f"- Billed time: ${billed_time}", end="", flush=True)
             print("\r", end="", flush=True)
         except KeyboardInterrupt:
             with open('report.csv', 'a+', newline='') as file:
@@ -55,6 +66,8 @@ def main(hourly_rate: float, activity: str,
                     writer.writerow(['requested_from', requested_from])
                     writer.writerow(['bill_to', bill_to])
                     writer.writerow(['total', total])
+                    writer.writerow(['date', date.today()])
+                    writer.writerow(['due_date', due_date])
 
             print("\nGood job")
             sys.exit()
